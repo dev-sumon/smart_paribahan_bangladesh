226a060e3595cb87377dd2cbcb869ac56d2d7154
dev-sumon <146063221+dev-sumon@users.noreply.github.com>
2024-12-03 20:14:06 +0600

    dashboard mastering done

---
+++ resources/views/backend/includes/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2024 <a href="https://dev-sumon.github.io/smart_poribahon/">Smart Paribahan Bangladesh</a>.</strong>
+    All rights reserved.
+  </footer>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +13,18 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+
+// Route::controller(UserSettingController::class)->prefix('settings')->name('us.')->group(function () {
+//     Route::get('profile', 'profile')->name('profile');
+//     Route::post('profile', 'profile_store')->name('profile');
+//     Route::get('change-password', 'password')->name('password');
+//     Route::post('change-password', 'password_store')->name('password');
+// });
+
+
+Route::controller(HomeController::class)->prefix('home')->name('home.')->group(function () {
+    Route::get('dashboard', 'dashboard')->name('dashboard');
+});
