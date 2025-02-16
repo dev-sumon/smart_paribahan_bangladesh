d90056e602e4e3a409e44927b81665ae9808fb37
dev-sumon <146063221+dev-sumon@users.noreply.github.com>
2025-02-16 13:11:24 +0600

    some change

--- resources/views/forntend/home/index.blade.php
+++ resources/views/forntend/home/index.blade.php
@@ -11,16 +11,14 @@
               <div class="title">
                 <h3>{{ __('আপনার নিরাপদ যাত্রার') }}</h3>
                 <h2>{{ __('স্মার্ট সলিউশন') }}</h2>
-                <p class="lead">
-                  সময় কারও জন্য অপেক্ষা না করলেও আপনার জীবনের সময় বাঁচাতে স্মার্ট সল্যুশন নিয়ে আপনার পাশে আমরা আছি
-                </p>
+                <p class="lead">{{ __('সময় কারও জন্য অপেক্ষা না করলেও আপনার জীবনের সময় বাঁচাতে স্মার্ট সল্যুশন নিয়ে আপনার পাশে আমরা আছি') }}</p>
               </div>
               <div class="row g-2">
                 <form action="">
                   <div class="row">
                     <div class="form-group col-4">
                       <select name="division_id" id="division" class="form-control form-select select_iteam">
-                        <option value="" selected hidden>বিভাগ</option>
+                        <option value="" selected hidden>{{ __('বিভাগ') }}</option>
                         @foreach ($divisions as $division)
                             <option value="{{ $division->id }}">{{ $division->division }}</option>
                         @endforeach
@@ -32,36 +30,36 @@
                     
                     <div class="form-group col-4">
                       <select name="district_id" id="district" class="form-select select_iteam">
-                        <option value="">জেলা</option>
+                        <option value="">{{ __('জেলা') }}</option>
                       </select>
                     </div>
                     
                     <div class="form-group col-4">
                       <select name="thana_id" id="thana" class="form-select select_iteam">
-                        <option value="">থানা</option>
+                        <option value="">{{ __('থানা') }}</option>
                       </select>
                     </div>
                 
                     <div class="form-group col-4">
                       <select name="union_id" id="union" class="form-select select_iteam">
-                        <option value="">ইউনিয়ন</option>
+                        <option value="">{{ __('ইউনিয়ন') }}</option>
                       </select>
                     </div>
                     
                     <div class="form-group col-4">
                       <select name="stand_id" id="stand" class="form-select select_iteam">
-                        <option value="">স্ট্যান্ড</option>
+                        <option value="">{{ __('স্ট্যান্ড') }}</option>
                       </select>
                     </div>
                     
                     <div class="form-group col-4">
                       <select name="vehicle_type_id" id="vehicle_type" class="form-select select_iteam">
-                        <option value="">গাড়ি</option>
+                        <option value="">{{ __('গাড়ি') }}</option>
                       </select>
                     </div>
                     
                     <div class="submit col-12">
-                      <button class="btn btn-custom mt-3 w-100">এখানে সার্চ করুন</button>
+                      <button class="btn btn-custom mt-3 w-100">{{ __('এখানে সার্চ করুন') }}</button>
                     </div>
                   </div>
                 </form>
@@ -88,7 +86,7 @@
                 <div class="details">
                   <a href="{{ route('f.cng.index') }}">
                     <img src="{{ asset('forntend/images/Group (6).svg') }}" alt="সি এন জি">
-                    <p>সি এন জি</p>
+                    <p>{{ __('সি এন জি') }}</p>
                   </a>
                 </div>
               </div>
@@ -98,7 +96,7 @@
                 <div class="details">
                   <a href="hiace.html">
                     <img src="{{ asset('forntend/images/Group (7).svg') }}" alt="হায়েস">
-                    <p>হায়েস</p>
+                    <p>{{ __('হায়েস') }}</p>
                   </a>
                 </div>
               </div>
@@ -108,7 +106,7 @@
                 <div class="details">
                   <a href="car.html">
                     <img src="{{ asset('forntend/images/Group (2).svg') }}" alt="কার">
-                    <p>কার</p>
+                    <p>{{ __('কার') }}</p>
                   </a>
                 </div>
               </div>
@@ -118,7 +116,7 @@
                 <div class="details">
                   <a href="micro.html">
                     <img src="{{ asset('forntend/images/Group (3).svg') }}" alt="মাইক্রো">
-                    <p>মাইক্রো</p>
+                    <p>{{ __('মাইক্রো') }}</p>
                   </a>
                 </div>
               </div>
@@ -128,7 +126,7 @@
                 <div class="details">
                   <a href="bike.html">
                     <img src="{{ asset('forntend/images/Group (4).svg') }}" alt="বাইক">
-                    <p>বাইক</p>
+                    <p>{{ __('বাইক') }}</p>
                   </a>
                 </div>
               </div>
@@ -138,7 +136,7 @@
                 <div class="details">
                   <a href="#">
                     <img src="{{ asset('forntend/images/Group (5).svg') }}" alt="পার্সেল">
-                    <p>পার্সেল</p>
+                    <p>{{ __('পার্সেল') }}</p>
                   </a>
                 </div>
               </div>
@@ -153,15 +151,15 @@
       <section class="connected_us" id="price-section">
         <div class="container py-5 pb-5">
             <div class="row g-2">
-                <h2 class="section-title mb-5">আমাদের সাথে যুক্ত আছেন</h2>
+                <h2 class="section-title mb-5">{{ __('আমাদের সাথে যুক্ত আছেন') }}</h2>
                 <!-- Card 1 -->
                 <div class="col-12 col-md-6 col-lg-3 mb-5 d-flex justify-content-center">
                     <div class="custom-card">
                         <div class="icon-container">
                             <img class="train" src="{{ asset('forntend/images/train.svg') }}" alt="Train Icon">
                         </div>
-                        <p class="card-text">সর্বমোট গাড়ির স্টেশন</p>
-                        <p class="price" data-original="78090">0</p>
+                        <p class="card-text">{{ __('সর্বমোট গাড়ির স্টেশন') }}</p>
+                        <p class="price" data-original="78090">{{ __('0') }}</p>
                     </div>
                 </div>
                 <!-- Card 2 -->
@@ -170,8 +168,8 @@
                         <div class="icon-container">
                             <img class="car" src="{{ asset('forntend/images/CAR 02 1.svg') }}" alt="Car Icon">
                         </div>
-                        <p class="card-text">সর্বমোট গাড়ি</p>
-                        <p class="price" data-original="90090">0</p>
+                        <p class="card-text">{{ __('সর্বমোট গাড়ি') }}</p>
+                        <p class="price" data-original="90090">{{ __('0') }}</p>
                     </div>
                 </div>
                 <!-- Card 3 -->
@@ -180,8 +178,8 @@
                         <div class="icon-container">
                             <img class="man" src="{{ asset('forntend/images/MAN 01 1.svg') }}" alt="Man Icon">
                         </div>
-                        <p class="card-text">সর্বমোট গাড়ির মালিক</p>
-                        <p class="price" data-original="78090">0</p>
+                        <p class="card-text">{{ __('সর্বমোট গাড়ির মালিক') }}</p>
+                        <p class="price" data-original="78090">{{ __('0') }}</p>
                     </div>
                 </div>
                 <!-- Card 4 -->
@@ -190,8 +188,8 @@
                         <div class="icon-container">
                             <img class="driver" src="{{ asset('forntend/images/DRIVER 1.svg') }}" alt="Driver Icon">
                         </div>
-                        <p class="card-text">সর্বমোট অভিজ্ঞ ড্রাইভার</p>
-                        <p class="price" data-original="78090">0</p>
+                        <p class="card-text">{{ __('সর্বমোট অভিজ্ঞ ড্রাইভার') }}</p>
+                        <p class="price" data-original="78090">{{ __('0') }}</p>
                     </div>
                 </div>
             </div>
@@ -208,7 +206,7 @@
           <div class="row d-flex align-items-center justify-content-center">
             <div class="col-12 col-md-4 d-flex align-items-center justify-content-sm-center">
               <div class="text">
-                <h2>সরাসরি গাড়ি অথবা <span class="d-none d-md-inline"><br></span> ড্রাইভার কে খুঁজুন</h2>
+                <h2>{{ __('সরাসরি গাড়ি অথবা') }}<span class="d-none d-md-inline"><br></span>{{ __('ড্রাইভার কে খুঁজুন') }}</h2>
               </div>
             </div>
             <div class="col-12 col-md-5">
@@ -218,7 +216,7 @@
             </div>
             <div class="col-12 justify-content-sm-center col-md-3 d-flex justify-content-end form">
               <form class="d-flex align-items-end" role="search">
-                <a href="error.html" class="btn btn-outline-success" type="submit">ক্লিক করুন</a>
+                <a href="error.html" class="btn btn-outline-success" type="submit">{{ __('ক্লিক করুন') }}</a>
               </form>
             </div>
           </div>
@@ -232,40 +230,38 @@
          <div class="row">
           <div class="help_us_headr text-center">
             <div class="title">
-              <h2>আমরা কিভাবে আপনাকে সহয়ায়তা করি</h2>
+              <h2>{{ __('আমরা কিভাবে আপনাকে সহয়ায়তা করি') }}</h2>
             </div>
             <div class="desc">
-              <p>
-                যেকোনো যাত্রী প্রয়োজনের সময় তার নিকটবর্তী স্টেশনে গাড়ির জন্য সিগন্যাল পাঠাতে <span class="d-none d-md-inline"><br></span> পারবেন।ফলে কাউকে ঘণ্টার পর ঘণ্টা গাড়ির জন্য অপেক্ষা করতে হবে না।
-              </p>
+              <p>{{ __('যেকোনো যাত্রী প্রয়োজনের সময় তার নিকটবর্তী স্টেশনে গাড়ির জন্য সিগন্যাল পাঠাতে') }}<span class="d-none d-md-inline"><br></span>{{ __('পারবেন।ফলে কাউকে ঘণ্টার পর ঘণ্টা গাড়ির জন্য অপেক্ষা করতে হবে না।') }}</p>
             </div>
           </div>
           <div class="car-container">
               <img src="{{ asset('forntend/images/Group 2.png') }}" alt="Car and Phone" class="car-image">
 
               <div class="feature-box feature-box-1">
-                  <span class="icon-circle-1">1</span>
-                  <p class="feature-text">আপনার কোনো কিছু গাড়িতে হারিয়ে গেলে আপনি এটা খুব সহজে খুঁজে বের করতে পারবেন।</p>
+                  <span class="icon-circle-1">{{ __('1') }}</span>
+                  <p class="feature-text">{{ __('আপনার কোনো কিছু গাড়িতে হারিয়ে গেলে আপনি এটা খুব সহজে খুঁজে বের করতে পারবেন।') }}</p>
               </div>
 
               <div class="feature-box feature-box-2">
-                  <span class="icon-circle-2">2</span>
-                  <p class="feature-text">যাত্রীরা নিরাপদেও স্মার্ট প্রযুক্তির মাধ্যমে চলাচল করতে পারবে।</p>
+                  <span class="icon-circle-2">{{ __('2') }}</span>
+                  <p class="feature-text">{{ __('যাত্রীরা নিরাপদেও স্মার্ট প্রযুক্তির মাধ্যমে চলাচল করতে পারবে।') }}</p>
               </div>
 
               <div class="feature-box feature-box-3">
-                  <span class="icon-circle-3">3</span>
-                  <p class="feature-text">যাত্রী যদি চায় তাহলে ড্রাইভারকে অনলাইনে পেমেন্ট করতে পারে যেমন বিকাশ, রকেট, নগদ অনলাইন সিস্টেমের মাধ্যমে।</p>
+                  <span class="icon-circle-3">{{ __('3') }}</span>
+                  <p class="feature-text">{{ __('যাত্রী যদি চায় তাহলে ড্রাইভারকে অনলাইনে পেমেন্ট করতে পারে যেমন বিকাশ, রকেট, নগদ অনলাইন সিস্টেমের মাধ্যমে।') }}</p>
               </div>
 
               <div class="feature-box feature-box-4">
-                  <span class="icon-circle-4">4</span>
-                  <p class="feature-text">যাত্রীদের কোনো কিছু যদি গাড়ির মধ্যে ফেলে যায়। তাহলে ড্রাইভার হারানো বিজ্ঞপ্তি দিতে পারবে।</p>
+                  <span class="icon-circle-4">{{ __('4') }}</span>
+                  <p class="feature-text">{{ __('যাত্রীদের কোনো কিছু যদি গাড়ির মধ্যে ফেলে যায়। তাহলে ড্রাইভার হারানো বিজ্ঞপ্তি দিতে পারবে।') }}</p>
               </div>
 
               <div class="feature-box feature-box-5">
-                  <span class="icon-circle-5">5</span>
-                  <p class="feature-text">প্রত্যেকটি গাড়ির মধ্যে QRকোড থাকবে।</p>
+                  <span class="icon-circle-5">{{ __('5') }}</span>
+                  <p class="feature-text">{{ __('প্রত্যেকটি গাড়ির মধ্যে QRকোড থাকবে।') }}</p>
               </div>
           </div>
          </div>
@@ -278,17 +274,17 @@
       <div class="container pt-5 pb-5">
         <div class="row d-flex align-items-stretch">
           <div class="head-text">
-            <h2 class="heading_section">বাংলাদেশ স্মার্ট পরিবহন ব্লগ থেকে </h2>
-            <p class="sub_text">সকল বিবৃতি, আপডেট, রিলিজ ও অন্যান্য</p>
+            <h2 class="heading_section">{{ __('বাংলাদেশ স্মার্ট পরিবহন ব্লগ থেকে') }}</h2>
+            <p class="sub_text">{{ __('সকল বিবৃতি, আপডেট, রিলিজ ও অন্যান্য') }}</p>
           </div>
           <!-- Blog Post 1 -->
           <div class="col-md-12 col-lg-4 mb-4 pb-4">
             <div class="post_card pb-4">
               <img src="{{ asset('forntend/images/smart_car.png') }}" alt="Blog 1" class="post_image">
               <div class="post_content">
-                <h4 class="post_title">স্মার্ট বাংলাদেশে পরিবহন খাত আনস্মার্টই থাকবে?</h4>
-                <p class="post_date"><i class="fa-regular fa-calendar mr-1" style="color: red;"></i>  জুন ৬, ২০২৪</p>
-                <p class="post_description">সমৃদ্ধ বাংলাদেশ গড়ে তুলতে সবার আগে প্রয়োজন আধুনিক যোগাযোগ ব্যবস্থা। সেটা হোক সড়ক, রেলপথ, নৌপথ ও তথ্যপ্রযুক্তি ব্যবস্থা।</p>
+                <h4 class="post_title">{{ __('স্মার্ট বাংলাদেশে পরিবহন খাত আনস্মার্টই থাকবে?') }}</h4>
+                <p class="post_date"><i class="fa-regular fa-calendar mr-1" style="color: red;"></i> {{ __('জুন ৬, ২০২৪') }}</p>
+                <p class="post_description">{{ __('সমৃদ্ধ বাংলাদেশ গড়ে তুলতে সবার আগে প্রয়োজন আধুনিক যোগাযোগ ব্যবস্থা। সেটা হোক সড়ক, রেলপথ, নৌপথ ও তথ্যপ্রযুক্তি ব্যবস্থা।') }}</p>
               </div>
             </div>
           </div>
@@ -297,9 +293,9 @@
             <div class="post_card">
               <img src="{{ asset('forntend/images/Location tracking-pana 1.png') }}" alt="Blog 2" class="post_image">
               <div class="post_content">
-                <h4 class="post_title">স্মার্ট বাংলাদেশে পরিবহন খাত আনস্মার্টই থাকবে?</h4>
-                <p class="post_date"><i class="fa-regular fa-calendar mr-1" style="color: red;"></i> জুন ৬, ২০২৪</p>
-                <p class="post_description">সমৃদ্ধ বাংলাদেশ গড়ে তুলতে সবার আগে প্রয়োজন আধুনিক যোগাযোগ ব্যবস্থা। সেটা হোক সড়ক, রেলপথ, নৌপথ ও তথ্যপ্রযুক্তি ব্যবস্থা।</p>
+                <h4 class="post_title">{{ __('স্মার্ট বাংলাদেশে পরিবহন খাত আনস্মার্টই থাকবে?') }}</h4>
+                <p class="post_date"><i class="fa-regular fa-calendar mr-1" style="color: red;"></i>{{ __('জুন ৬, ২০২৪') }}</p>
+                <p class="post_description">{{ __('সমৃদ্ধ বাংলাদেশ গড়ে তুলতে সবার আগে প্রয়োজন আধুনিক যোগাযোগ ব্যবস্থা। সেটা হোক সড়ক, রেলপথ, নৌপথ ও তথ্যপ্রযুক্তি ব্যবস্থা।') }}</p>
               </div>
             </div>
           </div>
@@ -308,15 +304,15 @@
             <div class="post_card">
               <img src="{{ asset('forntend/images/squti_Take Away-pana 1.png') }}" alt="Blog 3" class="post_image">
               <div class="post_content">
-                <h4 class="post_title">স্মার্ট বাংলাদেশে পরিবহন খাত আনস্মার্টই থাকবে?</h4>
-                <p class="post_date"><i class="fa-regular fa-calendar mr-1" style="color: red;"></i>  জুন ৬, ২০২৪</p>
-                <p class="post_description">সমৃদ্ধ বাংলাদেশ গড়ে তুলতে সবার আগে প্রয়োজন আধুনিক যোগাযোগ ব্যবস্থা। সেটা হোক সড়ক, রেলপথ, নৌপথ ও তথ্যপ্রযুক্তি ব্যবস্থা।</p>
+                <h4 class="post_title">{{ __('স্মার্ট বাংলাদেশে পরিবহন খাত আনস্মার্টই থাকবে?') }}</h4>
+                <p class="post_date"><i class="fa-regular fa-calendar mr-1" style="color: red;"></i> {{ __('জুন ৬, ২০২৪') }}</p>
+                <p class="post_description">{{ __('সমৃদ্ধ বাংলাদেশ গড়ে তুলতে সবার আগে প্রয়োজন আধুনিক যোগাযোগ ব্যবস্থা। সেটা হোক সড়ক, রেলপথ, নৌপথ ও তথ্যপ্রযুক্তি ব্যবস্থা।') }}</p>
               </div>
             </div>
           </div>
           <div class="btn_section">
             <a href="{{ route('f.blog.index') }}" class="gradient-border-button">
-              <span>সব গুলো ব্লগ</span>
+              <span>{{ __('সব গুলো ব্লগ') }}</span>
               <i class="fa-solid fa-arrow-right arrow"></i>
             </a>
           </div>
@@ -330,7 +326,7 @@
       <div class="container d-flex justify-content-center">
           <div class="row w-100">
             <div class="faq-section">
-              <div class="faq-title  py-5 pb-5">সচরাচর আপনার প্রশ্ন সমূহ</div>
+              <div class="faq-title  py-5 pb-5">{{ __('সচরাচর আপনার প্রশ্ন সমূহ') }}</div>
 
               @foreach ($faqs as $faq)
                 <div class="faq-item">
