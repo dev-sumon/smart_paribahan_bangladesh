65c6e33cb663b3346d74c21cc659380cb773665e
dev-sumon <146063221+dev-sumon@users.noreply.github.com>
2025-03-17 18:59:22 +0600

    search profile show done

--- resources/views/forntend/cng_info/search_info.blade.php
+++ resources/views/forntend/cng_info/search_info.blade.php
@@ -1,24 +1,119 @@
 @extends('forntend.layouts.master')
 @section('title', 'search profile')
 @section('content')
+<!-- president and members section start-->
+@foreach ($vehicles as $key=>$vehicle)
+<section class="president_and_members pt-5 pb-4">
+    <div class="container">
+        <div class="stand_location">
+            <h5>{{ $vehicle->driver->district->district }} - {{ $vehicle->driver->thana->thana }} - {{ $vehicle->driver->union->union }}</h5>
+        </div>
+        <div class="member_list_title">
+            <h2>{{ $vehicle->name }} {{ __('ড্রাইভার এর বিস্তারিত') }}</h2>
+        </div>
+    </div>
+</section>
 
-<!-- notice section start  -->
-<section class="notice">
-    <div class="container py-5">
-        <div class="row">
-            <div class="col-md-12 col-lg-9">
-                @foreach ($vehicles as $key=>$vehicle)
-                    
-                <h4>{{ $vehicle->vehicle_licence }}</h4>
-                @endforeach
+<!-- CNG Owner profile section start -->
+<div class="profile_section pt-5 pb-4">
+    <div class="container">
+        <div class="profile_deatils d-lg-flex d-md-block column-gap-5">
+            <div class="profile_image text-center">
+                <img src="{{ $vehicle->image ? asset('storage/' . $vehicle->image) : asset('forntend/images/Ellipse 199.png') }}" alt="Profile Image" class="profile-image mb-3 rounded-2">
             </div>
-            <div class="col-md-12 col-lg-9">
-                @foreach ($drivers as $key=>$driver)
-                    
-                <h4>{{ $driver->driving_license }}</h4>
-                @endforeach
+            <div class="profile_info">
+                <span class="owner">{{ __('আমি এর ড্রাইভার') }}</span>
+                <h3>{{ $vehicle->driver->name }}</h3>
+                <span class="desc">
+                  {{ $vehicle->driver->description }}
+                </span>
+                <div class="deatils d-flex column-gap-3 pt-3 align-items-center">
+                    <div class="email">{{ __('ইমেল - ') }}</div>
+                    <div class="number">{{ $vehicle->driver->email ?? 'ইমেল নম্বর পাওয়া যায়নি' }}</div>
+                </div>
+                <div class="deatils d-flex column-gap-3 pt-3 align-items-center">
+                    <div class="phone">{{ __('মোবাইল নাম্বার - ') }}</div>
+                    <div class="number">{{ $vehicle->driver->phone ?? 'ফোন নম্বর পাওয়া যায়নি' }}</div>
+                </div>
+                <div class="deatils d-flex column-gap-3 pt-3 align-items-center">
+                    <div class="car">{{ __('গাড়ির নাম্বার - ') }}</div>
+                    <div class="car_number">
+                        {{ $vehicle->vehicle_licence }}
+                    </div>
+                </div>
+                <div class="deatils d-flex column-gap-3 pt-3 align-items-center">
+                    <div class="blood">{{ __('ব্লাড গ্রুপ - ') }}</div>
+                    <div class="blood_group">{{ $vehicle->driver->blood_group->blood_group }}</div>
+                </div>
+                <div class="book_now d-none">
+                    <a href="">{{ __('গাড়ি বুক করুন') }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+ </div>
+<!-- CNG Owner profile section start -->
+<!-- video section start -->
+     
+<div class="video_section">
+    <div class="container text-center">
+        <div class="ratio ratio-16x9">
+            <iframe 
+                src="https://www.youtube.com/embed/qk7pZnxCX2o?si=sesWVnvaOcdsWI7h" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen>
+            </iframe>
+        </div>
+      </div>
+  </div>
+
+  <!-- video section start -->
+
+  <!-- gallery section start -->
+  <section class="gallery_section">
+    <div class="container pt-5 pb-5">
+        <div class="row">
+            <div class="d-none d-sm-flex mb-3">
+                <h3>{{ __('ইমেজ গ্যালারী - ') }}</h3>
+                <p class="ml-4">{{ $vehicle->driver->district->district }} - {{ $vehicle->driver->thana->thana }} - {{ $vehicle->driver->union->union }} - {{ $vehicle->driver->stand->name }} {{ __('স্ট্যান্ড এর') }}</p> 
+            </div>
+            
+            <div class="row gallery_image g-3">
+                <div class="col-6 col-md-4">
+                    <div class="">
+                        <img src="{{ asset('forntend/images/Rectangle 3842.png') }}" alt="" class="img-fluid">
+                    </div>
+                </div>
+                <div class="col-6 col-md-4">
+                    <div class="">
+                        <img src="{{ asset('forntend/images/Rectangle 3844.png') }}" alt="" class="img-fluid">
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class=""></div>
+                    <img src="{{ asset('forntend/images/Rectangle 3846.png') }}" alt="" class="img-fluid">
+                </div>
+            </div>
+        </div>
+        <div class="row pt-3">
+            <div class="row gallery_image g-3">
+                <div class="col-6 col-md-4">
+                    <img src="{{ asset('forntend/images/Rectangle 3847.png') }}" alt="" class="img-fluid">
+                </div>
+                <div class="col-6 col-md-4">
+                    <img src="{{ asset('forntend/images/Rectangle 3843.png') }}" alt="" class="img-fluid">
+                </div>
+                <div class="col-12 col-md-4">
+                    <img src="{{ asset('forntend/images/Rectangle 3845 (1).png') }}" alt="" class="img-fluid">
+                </div>
             </div>
         </div>
     </div>
 </section>
+   <!-- gallery section end -->
+
+@endforeach
 @endsection
