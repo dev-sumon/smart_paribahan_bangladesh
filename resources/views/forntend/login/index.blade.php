7364e1c5150d7c8a7c9b44307e9a14add0a57811
Md. Syful islam <mdsyfulislam20009@gmail.com>
2025-03-20 06:21:39 +0600

    login index translate
    login index translate

--- resources/views/forntend/login/index.blade.php
+++ resources/views/forntend/login/index.blade.php
@@ -18,9 +18,9 @@
     <section class="select_iteam">
         <div class="container d-flex justify-content-center align-items-center pt-5 py-5">
           <div class="card p-4 shadow-sm" style="max-width: 400px; width: 100%; border-radius: 15px">
-            <a href="{{ route('driver.login') }}">ড্রাইভার লগ ইন</a>
-            <a href="{{ route('owner.login') }}">মালিক লগ ইন</a>
-            <a href="">মাঠ কর্মী লগ ইন</a>
+            <a href="{{ route('driver.login') }}">{{ __('Driver Login') }}</a>
+            <a href="{{ route('owner.login') }}">{{ __('Owner Login') }}</a>
+            <a href="">{{ __('Field Worker Login') }}</a>
           </div>
         </div>
       </section>
