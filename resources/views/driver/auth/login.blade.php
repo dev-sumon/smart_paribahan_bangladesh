848e0c6d0c5df4858af6bf218dc3f490fe7659f4
dev-sumon <146063221+dev-sumon@users.noreply.github.com>
2025-05-10 16:26:11 +0600

    some update

--- resources/views/driver/auth/login.blade.php
+++ resources/views/driver/auth/login.blade.php
@@ -107,35 +107,25 @@
                         @enderror
                     </div>
                 </div>
-                {{-- <div class="mb-3">
-                    <label for="password" class="form-label">পাসওয়ার্ড</label>
-                    <input type="password" class="form-control" id="password" name="password" placeholder="পাসওয়ার্ড লিখুন" required />
-                    
-                </div> --}}
                 <div class="row mb-3">
-                    <label for="password" class="col-md-12 col-form-label">{{ __('পাসওয়ার্ড') }}</label>
-                    <div class="col-md-12">
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="পাসওয়ার্ড" name="password" required autocomplete="current-password">
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
+                        <label for="password" class="col-md-12 col-form-label">{{ __('পাসওয়ার্ড') }}</label>
+                        <div class="col-md-12" style="position: relative;">
+                            <input id="password" type="password"
+                                class="form-control @error('password') is-invalid @enderror" name="password"
+                                placeholder="পাসওয়ার্ড" required autocomplete="current-password"
+                                style="padding-right: 40px;">
+                            <span id="togglePassword"
+                                style="position: absolute; top: 50%; right: 25px; transform: translateY(-50%); cursor: pointer;">
+                                <i class="fa-solid fa-eye" id="eyeIcon"></i>
                             </span>
-                        @enderror
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
                     </div>
-                </div>
-                {{-- <div class="form-check mb-3 d-flex justify-content-between">
-                    <input class="form-check-input" type="checkbox" id="rememberMe" />
-                    <label class="form-check-label" for="rememberMe">{{ __('মনে রাখুন') }}</label>
-                    <a class="forget_password" href="">{{ __('পাসওয়ার্ড ভুলে গেছেন?') }}</a>
-                </div>
-                <button type="submit" class="btn btn-danger w-100 mb-3 login">{{ __('লগইন করুন') }}</button> --}}
                 <div class="text-center mb-3">
-                    {{-- <div class="divider">
-                    <hr>
-                    <span>{{ __('অথবা') }}</span>
-                    <hr>
-                    </div> --}}
-
                     <div class="form-check mb-3 d-flex justify-content-between">
                         <input class="form-check-input" type="checkbox" id="rememberMe" />
                         <label class="form-check-label" for="rememberMe">{{ __('মনে রাখুন') }}</label>
@@ -165,3 +155,28 @@
         </div>
     </section>
 @endsection
+
+
+@push('script')
+    <script>
+        const togglePassword = document.getElementById('togglePassword');
+        const passwordInput = document.getElementById('password');
+        const eyeIcon = document.getElementById('eyeIcon');
+
+        togglePassword.addEventListener('click', function() {
+            const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordInput.setAttribute('type', type);
+
+            // আইকন চেঞ্জ
+            if (type === 'text') {
+                eyeIcon.classList.remove('fa-eye');
+                eyeIcon.classList.add('fa-eye-slash');
+                eyeIcon.style.color = '#ea1827';
+            } else {
+                eyeIcon.classList.remove('fa-eye-slash');
+                eyeIcon.classList.add('fa-eye');
+                eyeIcon.style.color = '';
+            }
+        });
+    </script>
+@endpush
