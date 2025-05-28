bfadd634445ad88166dd76fcc1717245a61fa0fd
dev-sumon <146063221+dev-sumon@users.noreply.github.com>
2025-05-28 15:54:28 +0600

    driver updated done by filed worker

--- app/Http/Controllers/FieldWorker/FieldWorkerDriverController.php
+++ app/Http/Controllers/FieldWorker/FieldWorkerDriverController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\FieldWorker;
 
+use App\Models\Stand;
+use App\Models\Thana;
+use App\Models\Union;
 use App\Models\Driver;
 use App\Models\Vehicle;
+use App\Models\District;
 use App\Models\Division;
 use Illuminate\View\View;
 use App\Models\BloodGroup;
@@ -12,6 +16,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\DriverRequest;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
 
 class FieldWorkerDriverController extends Controller
 {
@@ -79,4 +84,75 @@ class FieldWorkerDriverController extends Controller
 
         return redirect()->route('field_worker.driver.index');
     }
+        public function update($slug): View
+    {
+        $data['driver'] = Driver::with('division', 'district', 'thana', 'union', 'stand', 'vehicle')->where('slug', $slug)->firstOrFail();
+        $data['divisions'] = Division::all();
+        $data['districts'] = District::where('division_id', $data['driver']->division_id)->get();
+        $data['thanas'] = Thana::where('district_id', $data['driver']->district_id)->get();
+        $data['unions'] = Union::where('thana_id', $data['driver']->thana_id)->get();
+        $data['stands'] = Stand::where('id', $data['driver']->stand_id)->get();
+        $data['vehicles'] = Vehicle::where('id', $data['driver']->vehicle_id)->get();
+        $data['bloods'] = BloodGroup::latest()->get();
+        return view('field_worker.driver.edit', $data);
+    }
+    public function update_store(DriverRequest $request, $slug): RedirectResponse
+    {
+        $update = Driver::where('slug',$slug)->firstOrFail();
+        $update->title = $request->title;
+
+        if ($update->isDirty('title')) {
+            $slug = Str::slug($request->title);
+            $originalSlug = $slug;
+            $count = 1;
+
+            while (Driver::where('slug', $slug)->where('id', '!=', $update->id)->exists()) {
+                $slug = $originalSlug . '-' . $count;
+                $count++;
+            }
+
+            $update->slug = $slug;
+        }
+
+        $update->description = $request->description;
+        $update->designation = $request->designation;
+        $update->email = $request->email;
+        $update->phone = $request->phone;
+        $update->owner_id = $request->owner_id;
+        $update->driving_license = $request->driving_license;
+        $update->blood_group_id = $request->blood_group_id;
+        $update->division_id = $request->division_id;
+        $update->district_id = $request->district_id;
+        $update->thana_id = $request->thana_id;
+        $update->union_id = $request->union_id;
+        $update->vehicle_id = $request->vehicle_id;
+        $update->stand_id = $request->stand_id;
+        $update->status = $request->status ?? 0;
+
+        if ($request->password) {
+            $update->password = $request->password;
+        }
+
+
+        if ($request->hasFile('image')) {
+            if ($update->image && Storage::exists($update->image)) {
+                Storage::delete($update->image);
+            }
+
+            $image = $request->file('image');
+            $filename = $request->name . time() . '.' . $image->getClientOriginalExtension();
+            $path = $image->storeAs("driver/", $filename, 'public');
+            $update->image = $path;
+        }
+
+
+        $update->save();
+
+        if ($request->vehicle_id) {
+            Vehicle::where('id', $request->vehicle_id)->update(['driver_id' => $update->id]);
+        }
+
+        return redirect()->route('field_worker.driver.index');
+    }
+
 }
