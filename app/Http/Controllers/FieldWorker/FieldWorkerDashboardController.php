ca490e1708f38a9270c4a017b8df06b3615a77b7
dev-sumon <146063221+dev-sumon@users.noreply.github.com>
2025-07-01 18:30:55 +0600

    dashboard update

--- app/Http/Controllers/FieldWorker/FieldWorkerDashboardController.php
+++ app/Http/Controllers/FieldWorker/FieldWorkerDashboardController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers\FieldWorker;
 
+use Carbon\Carbon;
 use App\Models\Blog;
+use App\Models\Owner;
+use App\Models\Stand;
+use App\Models\Union;
 use App\Models\Driver;
+use App\Models\Notice;
 use App\Models\Vehicle;
 use App\Models\Division;
 use App\Models\BloodGroup;
 use App\Models\FieldWorker;
+use App\Models\YearlyNotice;
 use Illuminate\Http\Request;
 use App\Http\Requests\BlogRequest;
 use Illuminate\Contracts\View\View;
@@ -26,12 +32,114 @@ class FieldWorkerDashboardController extends Controller
         $this->middleware('field_worker');
     }
 
-    public function dashboard(){
+    public function dashboard()
+    {
         $data['worker'] = Auth::guard('field_worker')->user();
+
+        // $data['stand_manager'] = Auth::guard('stand_manager')->user();
+        // $now = Carbon::now();
+
+        // $today = $now->copy()->startOfDay();
+        // $yesterday = $now->copy()->subDay()->startOfDay();
+
+        // $thisWeekStart = $now->copy()->startOfWeek();
+        // $lastWeekStart = $now->copy()->subWeek()->startOfWeek();
+        // $lastWeekEnd = $lastWeekStart->copy()->endOfWeek();
+
+        // $thisMonthStart = $now->copy()->startOfMonth();
+        // $lastMonthStart = $now->copy()->subMonth()->startOfMonth();
+        // $lastMonthEnd = $lastMonthStart->copy()->endOfMonth();
+
+        // $thisYearStart = $now->copy()->startOfYear();
+        // $lastYearStart = $now->copy()->subYear()->startOfYear();
+        // $lastYearEnd = $lastYearStart->copy()->endOfYear();
+
+        // $data = [];
+
+        // $data['today_serial'] = VehicleSerial::whereDate('created_at', $today)->count();
+        // $data['yesterday_serial'] = VehicleSerial::whereDate('created_at', $yesterday)->count();
+        // $data['serial_diff'] = $data['today_serial'] - $data['yesterday_serial'];
+
+        // // ✅ Advertisement হিসাব
+        // // $data['this_week_ads'] = Advertisement::whereBetween('created_at', [$thisWeekStart, $now])->count();
+        // // $data['last_week_ads'] = Advertisement::whereBetween('created_at', [$lastWeekStart, $lastWeekEnd])->count();
+        // // $data['ads_diff'] = $data['this_week_ads'] - $data['last_week_ads'];
+
+        // $data['this_month_notice'] = Notice::whereBetween('created_at', [$thisMonthStart, $now])->count();
+        // $data['last_month_notice'] = Notice::whereBetween('created_at', [$lastMonthStart, $lastMonthEnd])->count();
+        // $data['monthly_notice_diff'] = $data['this_month_notice'] - $data['last_month_notice'];
+
+        // $data['this_year_notice'] = Notice::whereBetween('created_at', [$thisYearStart, $now])->count();
+        // $data['last_year_notice'] = Notice::whereBetween('created_at', [$lastYearStart, $lastYearEnd])->count();
+        // $data['yearly_notice_diff'] = $data['this_year_notice'] - $data['last_year_notice'];
+
+
+
+        $now = Carbon::now();
+
+        $today = $now->copy()->startOfDay();
+        $yesterday = $now->copy()->subDay()->startOfDay();
+
+
+
+        $thisWeekStart = $now->copy()->startOfWeek();
+        $lastWeekStart = $now->copy()->subWeek()->startOfWeek();
+        $lastWeekEnd = $lastWeekStart->copy()->endOfWeek();
+
+
+        $thisMonthStart = $now->copy()->startOfMonth();
+        $lastMonthStart = $now->copy()->subMonth()->startOfMonth();
+        $lastMonthEnd = $lastMonthStart->copy()->endOfMonth();
+
+
+        $thisYearStart = $now->copy()->startOfYear();
+        $lastYearStart = $now->copy()->subYear()->startOfYear();
+        $lastYearEnd = $lastYearStart->copy()->endOfYear();
+
+
+
+
+        $data = [];
+
+
+        // driver
+        $data['today_driver'] = Driver::whereDate('created_at', $today)->count();
+        $data['yesterday_driver'] = Driver::whereDate('created_at', $yesterday)->count();
+        $data['driver_diff'] = $data['today_driver'] - $data['yesterday_driver'];
+
+        // owner
+        $data['today_owner'] = Owner::whereDate('created_at', $today)->count();
+        $data['yesterday_owner'] = Owner::whereDate('created_at', $yesterday)->count();
+        $data['owner_diff'] = $data['today_owner'] - $data['yesterday_owner'];
+
+
+        // stand
+        $data['this_week_stand'] = Stand::whereBetween('created_at', [$thisWeekStart, $now])->count();
+        $data['last_week_stand'] = Stand::whereBetween('created_at', [$lastWeekStart, $lastWeekEnd])->count();
+        $data['stand_diff'] = $data['this_week_stand'] - $data['last_week_stand'];
+
+
+        // union
+        $data['this_week_union'] = Union::whereBetween('created_at', [$thisWeekStart, $now])->count();
+        $data['last_week_union'] = Union::whereBetween('created_at', [$lastWeekStart, $lastWeekEnd])->count();
+        $data['union_diff'] = $data['this_week_union'] - $data['last_week_union'];
+
+        // notice
+        $data['this_month_notice'] = Notice::whereBetween('created_at', [$thisMonthStart, $now])->count();
+        $data['last_month_notice'] = Notice::whereBetween('created_at', [$lastMonthStart, $lastMonthEnd])->count();
+        $data['monthly_notice_diff'] = $data['this_month_notice'] - $data['last_month_notice'];
+
+
+        // notice year
+        $data['this_year_notice'] = YearlyNotice::whereBetween('created_at', [$thisYearStart, $now])->count();
+        $data['last_year_notice'] = YearlyNotice::whereBetween('created_at', [$lastYearStart, $lastYearEnd])->count();
+        $data['yearly_notice_diff'] = $data['this_year_notice'] - $data['last_year_notice'];
+
         return view('field_worker.dashboard.dashboard', $data);
     }
 
-    public function field_worker_update($id){
+    public function field_worker_update($id)
+    {
         $data['worker'] = Auth::guard('field_worker')->user();
         return view('field_worker.field_worker_update.index', $data);
     }
@@ -67,7 +175,7 @@ class FieldWorkerDashboardController extends Controller
     }
     public function driverCreate(): View
     {
-        $data['divisions'] = Division::with( ['districts', 'thanas', 'unions', 'stands', 'owners'])->latest()->get();
+        $data['divisions'] = Division::with(['districts', 'thanas', 'unions', 'stands', 'owners'])->latest()->get();
         $data['bloods'] = BloodGroup::latest()->get();
         return view('field_worker.pages.driver', $data);
     }
@@ -89,14 +197,14 @@ class FieldWorkerDashboardController extends Controller
         $save->stand_id = $request->stand_id;
         $save->driving_license = $request->driving_license;
         $save->password = Hash::make($request->password);
-    
-        if($request->hasFile('image')){
+
+        if ($request->hasFile('image')) {
             $image = $request->file('image');
-            $filename = $request->name . time(). '.' .$image->getClientOriginalExtension();
+            $filename = $request->name . time() . '.' . $image->getClientOriginalExtension();
             $path = $image->storeAs("driver/", $filename, 'public');
             $save->image = $path;
         }
-        
+
         $save->save();
 
         if ($request->vehicle_id) {
