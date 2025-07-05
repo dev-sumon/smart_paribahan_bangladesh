6c3a78e0428c6d1619da1894fe7bd163e0f00586
dev-sumon <146063221+dev-sumon@users.noreply.github.com>
2025-07-05 15:14:12 +0600

    create and index page data show done

--- app/Http/Controllers/FieldWorker/FieldWorkerYearlyNoticeController.php
+++ app/Http/Controllers/FieldWorker/FieldWorkerYearlyNoticeController.php
@@ -2,18 +2,66 @@
 
 namespace App\Http\Controllers\FieldWorker;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Stand;
+use App\Models\Thana;
+use App\Models\Union;
+use App\Models\District;
+use App\Models\Division;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
+use App\Models\YearlyNotice;
+use Illuminate\Http\Request;
+use App\Models\NoticeCategory;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\YearlyNoticeRequest;
 
 class FieldWorkerYearlyNoticeController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('field_worker');
-    // }
-    public function index():View
+    public function __construct()
     {
-        return view('field_worker.yearly_notice.index');
+        $this->middleware('field_worker');
+    }
+    public function index(): View
+    {
+        $data['yearly_notices'] = YearlyNotice::latest()->get();
+        return view('field_worker.yearly_notice.index', $data);
+    }
+    public function create(): View
+    {
+        $data['divisions'] = Division::latest()->get();
+        $data['districts'] = District::latest()->get();
+        $data['thanas'] = Thana::latest()->get();
+        $data['unions'] = Union::latest()->get();
+        $data['stands'] = Stand::latest()->get();
+        $data['categories'] = NoticeCategory::latest()->get();
+        return view('field_worker.yearly_notice.create', $data);
+    }
+    public function store(YearlyNoticeRequest $request): RedirectResponse
+    {
+        $save = new YearlyNotice();
+
+        $save->division_id = $request->division_id;
+        $save->district_id = $request->district_id;
+        $save->thana_id = $request->thana_id;
+        $save->union_id = $request->union_id;
+        $save->stand_id = $request->stand_id;
+        $save->title = $request->title;
+        $save->date = $request->date;
+        $save->notice_category_id = $request->notice_category_id;
+        $save->status = $request->status ?? 0;
+
+        if ($request->hasFile('file')) {
+            $file = $request->file('file');
+            $filename = $request->name . time() . '.' . $file->getClientOriginalExtension();
+            $path = $file->storeAs("notices/", $filename, 'public');
+            $save->file = $path;
+        }
+
+
+        $save->created_by_id = Auth::guard('field_worker')->id();
+        $save->created_by_guard = 'field_worker';
+        $save->save();
+        return redirect()->route('field_worker.yearly_notice.index')->with('success','Yearly created successfully by Field Worker');
     }
 }
---
+++ resources/views/field_worker/yearly_notice/create.blade.php
@@ -0,0 +1,161 @@
+@extends('field_worker.layouts.master', ['page_slug' => 'yearly_notice'])
+@section('title', 'Notice Create')
+@section('content')
+    <div class="container-fluid mt-2">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between">
+                        <span class="float-left card-title">
+                            <h4>{{ __('Create Yearly Notice') }}</h4>
+                        </span>
+                        <span class="float-right">
+                            <a href="{{ route('field_worker.yearly_notice.index') }}" class="btn btn-info">{{ __('Back') }}</a>
+                        </span>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-10 m-auto">
+                                <form action="{{ route('field_worker.yearly_notice.store') }}" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="form-group">
+                                        <label for="division">{{ __('Division') }}<span class="text-danger">*</span></label>
+                                        <select name="division_id" id="division" class="form-control">
+                                            <option value="" selected hidden>{{ __('Select Division') }}</option>
+                                            @foreach ($divisions as $division)
+                                                <option value="{{ $division->id }}"
+                                                    {{ $division->id == old('division_id') ? 'selected' : '' }}>
+                                                    {{ $division->division }}</option>
+                                            @endforeach
+                                        </select>
+                                        @if ($errors->has('division_id'))
+                                            <div class="text-danger">{{ $errors->first('division_id') }}</div>
+                                        @endif
+                                    </div>
+                                    <div class="form-group mt-3">
+                                        <label for="district">{{ __('District') }}<span
+                                                class="text-danger">*</span></label>
+                                        <select name="district_id" id="district" class="form-control">
+                                            <option value="" selected hidden>{{ __('Select District') }}</option>
+                                            @foreach ($districts as $district)
+                                                <option value="{{ $district->id }}"
+                                                    {{ $district->id == old('district_id') ? 'selected' : '' }}>
+                                                    {{ $district->district }}</option>
+                                            @endforeach
+                                        </select>
+                                        @if ($errors->has('district_id'))
+                                            <div class="text-danger">{{ $errors->first('district_id') }}</div>
+                                        @endif
+                                    </div>
+
+                                    <div class="form-group mt-3">
+                                        <label for="thana">{{ __('Thana') }}<span class="text-danger">*</span></label>
+                                        <select name="thana_id" id="thana" class="form-control">
+                                            <option value="" selected hidden>{{ __('Select Thana') }}</option>
+                                            @foreach ($thanas as $thana)
+                                                <option value="{{ $thana->id }}"
+                                                    {{ $thana->id == old('thana_id') ? 'selected' : '' }}>
+                                                    {{ $thana->thana }}</option>
+                                            @endforeach
+                                        </select>
+                                        @if ($errors->has('thana_id'))
+                                            <div class="text-danger">{{ $errors->first('thana_id') }}</div>
+                                        @endif
+                                    </div>
+                                    <div class="form-group mt-3">
+                                        <label for="union">{{ __('Union') }}<span class="text-danger">*</span></label>
+                                        <select name="union_id" id="union" class="form-control">
+                                            <option value="" selected hidden>{{ __('Select Union') }}</option>
+                                            @foreach ($unions as $union)
+                                                <option value="{{ $union->id }}"
+                                                    {{ $union->id == old('union_id') ? 'selected' : '' }}>
+                                                    {{ $union->union }}</option>
+                                            @endforeach
+                                        </select>
+                                        @if ($errors->has('union_id'))
+                                            <div class="text-danger">{{ $errors->first('union_id') }}</div>
+                                        @endif
+                                    </div>
+                                    <div class="form-group mt-3">
+                                        <label for="stand">{{ __('Stand') }}<span class="text-danger">*</span></label>
+                                        <select name="stand_id" id="stand" class="form-control">
+                                            <option value="" selected hidden>{{ __('Select Stand') }}</option>
+                                            @foreach ($stands as $stand)
+                                                <option value="{{ $stand->id }}"
+                                                    {{ $stand->id == old('stand_id') ? 'selected' : '' }}>
+                                                    {{ $stand->title }}</option>
+                                            @endforeach
+                                        </select>
+                                        @if ($errors->has('stand_id'))
+                                            <div class="text-danger">{{ $errors->first('stand_id') }}</div>
+                                        @endif
+                                    </div>
+                                    <div class="form-group mt-3">
+                                        <label for="title">{{ __('Title') }} <span
+                                                class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" id="title"
+                                            placeholder="Enter The Notice Title" name="title"
+                                            value="{{ old('title') }}">
+                                        @if ($errors->has('title'))
+                                            <div class="text-danger">{{ $errors->first('title') }}</div>
+                                        @endif
+                                    </div>
+                                    <div class="form-group mt-3">
+                                        <label for="date">{{ __('Date') }} <span
+                                                class="text-danger">*</span></label>
+                                        <input type="date" name="date" value="{{ old('date') }}"
+                                            class="form-control" placeholder="Enter The Date">
+                                        @if ($errors->has('date'))
+                                            <div class="text-danger">{{ $errors->first('date') }}</div>
+                                        @endif
+                                    </div>
+                                    <div class="form-group mt-3">
+                                        <label for="category">{{ __('Category') }} <span
+                                                class="text-danger">*</span></label>
+                                        <select name="notice_category_id" id="notice_category_id" class="form-control">
+                                            <option value="" selected hidden>{{ __('Select Notice Category') }}
+                                            </option>
+                                            @foreach ($categories as $category)
+                                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                            @endforeach
+                                        </select>
+                                        @if ($errors->has('category'))
+                                            <div class="text-danger">{{ $errors->first('category') }}</div>
+                                        @endif
+                                    </div>
+                                    <div class="form-group mt-3">
+                                        <label for="file">{{ __('file') }} <span
+                                                class="text-danger">*</span></label>
+                                        <input type="file" name="file" value="{{ old('file') }}"
+                                            class="form-control h-auto" placeholder="Enter The File">
+                                        @if ($errors->has('file'))
+                                            <div class="text-danger">{{ $errors->first('file') }}</div>
+                                        @endif
+                                    </div>
+                                    <div class="form-group mt-3">
+                                        <label for="status">{{ __('Status') }} <span
+                                                class="text-danger">*</span></label>
+                                        <select name="status" id="status" class="form-control">
+                                            <option value="1" {{ old('status') == 1 ? 'selected' : '' }}>
+                                                {{ __('Active') }}</option>
+                                            <option value="0" {{ old('status') == 0 ? 'selected' : '' }}>
+                                                {{ __('Deactive') }}</option>
+                                        </select>
+                                        @if ($errors->has('status'))
+                                            <div class="text-danger">{{ $errors->first('status') }}</div>
+                                        @endif
+                                    </div>
+                                    <div class="form-group mt-3">
+                                        <button type="submit" class="btn btn-success w-100 submitBtn">
+                                            {{ __('Submit') }}
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
